6ab53c47617226634e58a083490cfe2fd415bf8a
razi-king <raziinsane@gmail.com>
2025-08-03 19:13:29 +0530

    Basic Set Up Done

---
+++ backend/smart-vendor-app-backend/src/main/java/com/example/smartvendorapp/SmartVendorAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartvendorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVendorAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartVendorAppBackendApplication.class, args);
+	}
+
+}
